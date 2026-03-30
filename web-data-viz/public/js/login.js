500ccb477a4144c629e893449d97c9040795632b
pedrodeca02 <pedro.carlos@sptech.school>
2026-03-29 23:19:18 -0300

    Arrumando o js do login

--- web-data-viz/public/js/login.js
+++ web-data-viz/public/js/login.js
@@ -1,71 +1,38 @@
-function aguardar() {
-    div_aguardar.style.display = "block";
-}
-
-function finalizarAguardar() {
-    div_aguardar.style.display = "none";
-}
-
-function mostrarErro(msg) {
-    cardErro.style.display = "block";
-    mensagem_erro.innerHTML = msg;
-
-    setTimeout(() => {
-        cardErro.style.display = "none";
-    }, 4000);
-}
-
 function entrar() {
-
-    aguardar();
 
     var email = email_input.value;
     var senha = senha_input.value;
 
     if (email == "" || senha == "") {
-        finalizarAguardar();
-        mostrarErro("Preencha todos os campos!");
-        return false;
+        alert('Preencha todos os campos');
+        return;
     }
 
-    fetch("/usuarios/auth", {
+    fetch("/users/auth", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            email: email,
+            email: email,   
             password: senha
         })
     })
-    .then(function(resposta) {
+    .then(res => {
+        if (!res.ok) throw new Error("Erro login");
 
-        if (resposta.ok) {
-
-            resposta.json().then(json => {
-
-                // sessão
-                sessionStorage.EMAIL_USUARIO = json.email;
-                sessionStorage.NOME_USUARIO = json.name;
-                sessionStorage.ID_USUARIO = json.id;
-
-                setTimeout(() => {
-                    window.location = "dashboard.html";
-                }, 1000);
-
-            });
-
-        } else {
-            throw "Erro no login";
-        }
-
-        finalizarAguardar();
+        return res.json();
     })
-    .catch(function(erro) {
-        console.log(erro);
-        finalizarAguardar();
-        mostrarErro("Email ou senha inválidos!");
+    .then(json => {
+
+        sessionStorage.ID_USUARIO = json.id;
+
+        window.location.href = "./dashboard/dashboard.html";
+
+    })
+    .catch(err => {
+        console.log(err);
+        alert("Erro no login");
     });
 
-    return false;
 }
