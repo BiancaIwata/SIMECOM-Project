551ac9589d95b1c2fceeafd368333d9bf11aa116
D-Foschini <daniel.foschini@sptech.school>
2026-04-10 15:05:58 -0300

    Funcionalidades da tela de configurações de perfil

--- web-data-viz/public/js/login.js
+++ web-data-viz/public/js/login.js
@@ -1,38 +1,37 @@
 function entrar() {
+  var email = email_input.value;
+  var senha = senha_input.value;
 
-    var email = email_input.value;
-    var senha = senha_input.value;
+  if (email == "" || senha == "") {
+    alert("Preencha todos os campos");
+    return;
+  }
 
-    if (email == "" || senha == "") {
-        alert('Preencha todos os campos');
-        return;
-    }
+  fetch("/users/auth", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: senha,
+    }),
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error("Erro login");
 
-    fetch("/users/auth", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            email: email,   
-            password: senha
-        })
+      return res.json();
     })
-    .then(res => {
-        if (!res.ok) throw new Error("Erro login");
-
-        return res.json();
-    })
-    .then(json => {
-
+    .then((json) => {
+      if (json.status == "inativo") {
+        alert("Conta está desativada...");
+      } else {
         sessionStorage.ID_USUARIO = json.id;
-
-        window.location.href = "./dashboard/dashboard.html";
-
+        window.location.href = "./dashboard.html";
+      }
     })
-    .catch(err => {
-        console.log(err);
-        alert("Erro no login");
+    .catch((err) => {
+      console.log(err);
+      alert("Erro no login");
     });
-
 }
