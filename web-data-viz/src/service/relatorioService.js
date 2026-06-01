0fec21bcb38340466501f19be101355cbdb9ad35
D-Foschini <daniel.foschini@sptech.school>
2026-06-01 19:36:45 -0300

    fixes do diretorio

--- web-data-viz/src/service/relatorioService.js
+++ web-data-viz/src/service/relatorioService.js
@@ -3,7 +3,8 @@ const { exec } = require("child_process");
 function gerarRelatorio() {
   return new Promise((resolve, reject) => {
     exec(
-      "java -jar ../../simecom-report-app/target/comex-report-app-1.0.0-jar-with-dependencies.jar filtros-exemplo.json",
+      "java -jar ../simecom-report-app/target/comex-report-app-1.0.0-jar-with-dependencies.jar filtros-exemplo.json",
+
       (error, stdout, stderr) => {
         if (error) {
           reject(error);
